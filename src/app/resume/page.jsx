a0c8bac6dd6e7bf5ba3155738b49ad7f0b85d0c5
VIKASH KUMAR <itsvikash143@gmail.com>
2025-03-07 10:11:20 +0530

    Fix case sensitivity issue

--- src/app/resume/page.jsx
+++ src/app/resume/page.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import Photo1 from "@/components/Photo1";
 import { FiDownload } from "react-icons/fi";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/Card";
 
 
 
